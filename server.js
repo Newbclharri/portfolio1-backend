51e32c17a6604f653b3bc16ba465da3682a5c733
contactharrisc2 <contactharrisc2@gmail.com>
2023-07-22 05:59:05 -0500

    adds nodemailer service

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ require("dotenv").config();
 
 const express = require('express'); //server framework
 const cors = require('cors'); //cross origin resource sharing to access data from foreign origins
+const nodemailer = require("nodemailer");
+
+const aws = require("@aws-sdk/client-ses");
+const defaultProvider = require("@aws-sdk/credential-provider-node");
+const ses = new aws.SES({
+    apiVersion: "2010-12-01",
+    region: "us-east-1",
+    defaultProvider
+});
 
 /////////////////////
 // JSON FILES
@@ -21,6 +30,7 @@ const PORT = process.env.PORT || 4000;
 // MIDDLEWARE
 //////////////
 app.use(cors());
+app.use(express.json()); //receive json
 
 //////////////
 // ROUTES
@@ -48,6 +58,49 @@ app.get("/tutorials", (req, res)=>{
     //send about via JSON
     res.json(tutorials);
 });
+
+app.post("/contact", (req, res)=>{
+    const contents = req.body;
+    console.log(contents)
+    console.log(contents.message)
+    // sendEmail(contents)
+    //     .then(()=>{res.send("success")})
+    //     .catch(err => res.send(err.message));
+    // if(!contents){
+    //     return res.status(400).send({status: "failed"});
+    // } 
+});
+
+//////////////////
+// Nodemailer fxns
+//////////////////
+function sendEmail(obj){
+    console.log(typeof(obj))
+    return new Promise((resolve, reject)=>{
+        const transporter = nodemailer.createTransport({
+            SES: {ses,aws},
+        });
+        const mailConfigs = {
+            from: EMAIL, // sender address
+            to: EMAIL, // list of receivers
+            subject: obj.subject,
+            text: obj.message
+
+        };
+        transporter.sendMail(mailConfigs,(err, info)=>{
+            if(err){
+                console.log("Error: ", err);
+                reject({message: "An error occurred"});
+            }else{
+                console.log('Email sent ' + info.response);
+                resolve({message: "email sent successfuly"});
+            }
+            
+        });  
+        
+    });
+        
+}
 
 /////////////
 // PORT LISTENER
